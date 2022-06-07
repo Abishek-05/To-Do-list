0bbfc9fd04f33630acba2994f5f45164e84559ca
Abishek <106460067+Abishek-05@users.noreply.github.com>
2022-06-07 13:17:26 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,97 @@
+function addfunc()
+{
+	tit = document.getElementById("title").value;
+	desc = document.getElementById("description").value;
+	
+	//If some elements are already present, first store it
+	if(localStorage.getItem("itemsJson") != null)
+	{
+		str = localStorage.getItem("itemsJson"); //JSON will return string
+		itemsJsonarr = JSON.parse(str);
+	}
+	//Add new item to array and then set to local storage
+	itemsJsonarr.push([tit,desc]);
+	localStorage.setItem("itemsJson",JSON.stringify(itemsJsonarr));
+	console.log("str",JSON.stringify(itemsJsonarr));
+	console.log("Item added");
+
+	document.getElementById("title").value="";
+	document.getElementById("description").value="";
+}
+function buildTable(arr)
+{
+	tableBody.innerHTML="";
+	for(i=0; i<arr.length; i++)
+	{
+		// if(arr[i][0] == "")
+		if(arr[i][0] == null)
+			continue;
+		row=`<tr>
+				<td>${i+1}</td>
+				<td>${arr[i][0]}</td>
+				<td>${arr[i][1]}</td>
+				<td><button id="${i}" class="btn btn-sm delData" onclick="delfunc(id)">Delete</button></td>
+		</tr>`
+		tableBody.innerHTML += row;
+	}
+	console.log("Table built");
+}
+function delfunc(pos) //pos wrt to itemJsonarr
+{
+	itemsJsonarr.splice(pos,1); //Deleting from itemJsonarr
+	localStorage.setItem("itemsJson",JSON.stringify(itemsJsonarr));
+
+	//Redirect the changes in table
+	buildTable(itemsJsonarr);
+	console.log("Element deleted");
+}
+function clrfunc()
+{
+	if(confirm("Do you want to clear the list ? "))
+	{
+		localStorage.clear();
+		tableBody.innerHTML="";
+		itemsJsonarr=[];
+		console.log("List cleared");
+	}
+}
+function srchfunc()
+{
+	//The string which user is typing
+	srchstr = document.getElementById("srch").value;
+	//Iterate through itemsJsonarr to find matches
+	newarr=[];
+	for(var i=0; i<itemsJsonarr.length; i++)
+	{
+		curr_title = itemsJsonarr[i][0];
+		curr_desc =  itemsJsonarr[i][1];
+		if(curr_title.startsWith(srchstr,0))
+			newarr.push(itemsJsonarr[i]);
+		else
+			// newarr.push(["",""]);
+			newarr.push([null,null]);
+	}
+	buildTable(newarr);
+}
+itemsJsonarr = [];
+//Populate table with local storage data....This is the first step always
+if(localStorage.getItem("itemsJson") != null)
+{
+	content = localStorage.getItem("itemsJson");
+	itemsJsonarr = JSON.parse(content);
+	buildTable(itemsJsonarr);
+}
+tableBody= document.getElementById("tableBody");
+
+//Adding elements
+add = document.getElementById("add");
+add.addEventListener("click",addfunc);
+add.addEventListener("click",() => {
+	buildTable(itemsJsonarr)
+});
+
+clr = document.getElementById("clearlist");
+clr.addEventListener("click",clrfunc);
+
+srch = document.getElementById("srch");//string which user is searching
+srch.addEventListener("keyup",srchfunc);
